e115840d82214fabab11ca7b5242516e43bbb99e
agyesam12 <agyesam12@gmail.com>
2025-04-25 15:36:34 +0000

    added backends.py file

---
+++ authentication/backends.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+from django.db import models
+
+
+User = get_user_model()
+
+class EmailOrPhoneBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(
+                models.Q(phone_number=username) | models.Q(email=username)
+            )
+        except User.DoesNotExist:
+            return None
+
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
+        return None
