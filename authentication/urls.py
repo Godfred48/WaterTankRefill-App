cbb019f0ca0bc105a4b2a0602a9a78bbaa20951d
agyesam12 <agyesam12@gmail.com>
2025-04-26 01:24:29 +0000

    added the signup views

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import *
 
 urlpatterns = [
     path('', views.home, name = 'home'),
@@ -9,4 +10,10 @@ urlpatterns = [
     path('services/', views.services, name = 'services'),
     path('signup/', views.signup, name = 'signup'),
     path('orders/', views.orders, name= 'orders'),
+    # testing paths
+    path('test/signin/', LoginView.as_view(), name= 'test/login'),
+    path('test/signup/', SignUpView.as_view(), name= 'test/signup'),
+    path('Logout/', LogoutView.as_view(), name = 'logout'),
+    path('vendor_dashboard/', views.vendor_dashboard, name='vendor_dashboard'),
+
 ]
