aec2849a5e5e7908b34d8899e2e3facd5951b131
agyesam12 <agyesam12@gmail.com>
2025-04-28 23:01:24 +0000

    updated the urls path

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,16 +3,15 @@ from . import views
 from .views import *
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('signin/', views.signin, name='signin'),
+    path('', HomeView.as_view(), name = 'home'),
+    
     path('customer_dashboard/', views.customer_dashboard, name='customer_dashboard'),
     path('about/', views.about, name = 'about'),
     path('services/', views.services, name = 'services'),
-    path('signup/', views.signup, name = 'signup'),
     path('orders/', views.orders, name= 'orders'),
     # testing paths
-    path('test/signin/', LoginView.as_view(), name= 'test/login'),
-    path('test/signup/', SignUpView.as_view(), name= 'test/signup'),
+    path('signin/', LoginView.as_view(), name= 'login'),
+    path('signup/', SignUpView.as_view(), name= 'signup'),
     path('Logout/', LogoutView.as_view(), name = 'logout'),
     path('vendor_dashboard/', views.vendor_dashboard, name='vendor_dashboard'),
     path('vendor/unboard_driver/', DriverOnboardingView.as_view(), name='onboard_drive'),
