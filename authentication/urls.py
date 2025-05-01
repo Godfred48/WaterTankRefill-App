58e99641491b60dd9a640bcc57f56c91db9b4456
agyesam12 <agyesam12@gmail.com>
2025-05-01 14:38:22 +0000

    updated the urls path

--- authentication/urls.py
+++ authentication/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('vendor/orders/', VendorViewOrders.as_view(), name='vendor_orders'),
     #customer orders
     path('customer/orders/', CustomerViewOrders.as_view(), name='customer_orders'),
+    path('customer/payments/', CustomerPaymentListView.as_view(), name='customer_payments'),
     path('customer/profile/', CustomerProfileView.as_view(), name='customer_profile'),
     path('customer/profile/update/', CustomerProfileUpdateView.as_view(), name='customer_profile_update'),
     path('profile/<str:user_id>/', CustomerProfileDetailView.as_view(), name='customer_profile_detail'),
