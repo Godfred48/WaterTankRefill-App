44116d2966ca94635e54dcf15cf51e20c15eef55
agyesam12 <agyesam12@gmail.com>
2025-05-02 00:00:00 +0000

    configured asgi

--- WaterTankRefillSystem/asgi.py
+++ WaterTankRefillSystem/asgi.py
@@ -8,9 +8,24 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import authentication.routing
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WaterTankRefillSystem.settings')
 
 application = get_asgi_application()
+
+
+
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            authentication.routing.websocket_urlpatterns
+        )
+    ),
+})
