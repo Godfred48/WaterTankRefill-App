34751e421f76fa0db0a845861bb3d8e2c82930df
agyesam12 <agyesam12@gmail.com>
2025-05-03 16:30:46 +0000

    Updated the consumers.py file

--- authentication/consumers.py
+++ authentication/consumers.py
@@ -1,52 +1,57 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from .models import Delivery
+from channels.db import database_sync_to_async
+from django.contrib.auth import get_user_model
 
-class DeliveryTrackingConsumer(AsyncWebsocketConsumer):
+User = get_user_model()
+
+class LiveLocationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.delivery_id = self.scope['url_route']['kwargs']['delivery_id']
-        self.room_group_name = f'delivery_{self.delivery_id}'
+        if self.scope["user"].is_anonymous:
+            await self.close()
+        else:
+            self.user = self.scope["user"]
+            self.group_name = "live_tracking"
 
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-        await self.accept()
+            # Add user to tracking group
+            await self.channel_layer.group_add(
+                self.group_name,
+                self.channel_name
+            )
+            await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
 
     async def receive(self, text_data):
         data = json.loads(text_data)
-        lat = data['lat']
-        lng = data['lng']
-        role = data['role']
+        latitude = data.get("latitude")
+        longitude = data.get("longitude")
 
-        # Broadcast location to the room
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'location_update',
-                'role': role,
-                'lat': lat,
-                'lng': lng
-            }
-        )
+        if latitude is not None and longitude is not None:
+            await self.channel_layer.group_send(
+                self.group_name,
+                {
+                    "type": "send_location",
+                    "user_id": self.user.id,
+                    "full_name": await self.get_full_name(),
+                    "latitude": latitude,
+                    "longitude": longitude,
+                }
+            )
 
-        # Optionally update DB (skip if you want minimal writes)
-        delivery = await self.get_delivery()
-        if role == "driver":
-            delivery.driver_current_lat = lat
-            delivery.driver_current_lng = lng
-        elif role == "customer":
-            delivery.customer_lat = lat
-            delivery.customer_lng = lng
-        await database_sync_to_async(delivery.save)()
-
-    async def location_update(self, event):
-        await self.send(text_data=json.dumps({
-            'role': event['role'],
-            'lat': event['lat'],
-            'lng': event['lng']
-        }))
+    async def send_location(self, event):
+        # Don't send your own location back to yourself
+        if event["user_id"] != self.user.id:
+            await self.send(text_data=json.dumps({
+                "full_name": event["full_name"],
+                "latitude": event["latitude"],
+                "longitude": event["longitude"],
+            }))
 
     @database_sync_to_async
-    def get_delivery(self):
-        return Delivery.objects.get(delivery_id=self.delivery_id)
+    def get_full_name(self):
+        return self.user.full_name
