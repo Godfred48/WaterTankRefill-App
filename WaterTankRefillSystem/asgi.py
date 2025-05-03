c2c126e481ed31be37162b7afc7ca5a2c697224a
agyesam12 <agyesam12@gmail.com>
2025-05-03 16:27:28 +0000

    updated settings.py to configure channels

--- WaterTankRefillSystem/asgi.py
+++ WaterTankRefillSystem/asgi.py
@@ -1,31 +1,23 @@
-"""
-ASGI config for WaterTankRefillSystem project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-import authentication.routing
+import authentication.routing  # Your app's routing.py
 
+import os
 from django.core.asgi import get_asgi_application
 
+# Make sure settings are properly configured
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WaterTankRefillSystem.settings')
 
 application = get_asgi_application()
-
-
 
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            authentication.routing.websocket_urlpatterns
+            authentication.routing.websocket_urlpatterns  # <-- Make sure this matches
         )
     ),
 })
