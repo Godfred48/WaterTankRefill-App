597080ff6e8c419b15a6497c821c59a0c0642f0d
agyesam12 <agyesam12@gmail.com>
2025-05-05 20:54:15 +0000

    updated the urls path

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .views import *
+from django.contrib.auth.views import PasswordResetView,PasswordResetDoneView,PasswordResetConfirmView,PasswordResetCompleteView
 
 urlpatterns = [
     path('', HomeView.as_view(), name = 'home'),
@@ -49,5 +50,11 @@ urlpatterns = [
     path('post-location/', views.update_location, name='post_location'),
     #path('track/', views.driver_track_delivery_view, name='track_delivery'),
     path('update-location/', views.update_location, name='update_location'),
+
+    #password reset stuffs
+    path('password-reset/', PasswordResetView.as_view(template_name='password_reset.html'),name='password-reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='password_reset_done.html'),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),name='password_reset_confirm'),
+    path('password-reset-complete/',PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),name='password_reset_complete'),
 
 ]
